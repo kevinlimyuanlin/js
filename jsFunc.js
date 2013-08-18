05aaa34364940553377f051dd5967ae10240b108
Kevin Lim <kevin_raffles@hotmail.com>
2013-08-18 09:02:16 +0000

    string error?

--- jsFunc.js
+++ jsFunc.js
@@ -7,8 +7,8 @@ function write2Para(paraID, text) {
 }
 
 function writeTemp(paraID, cTemp) {
-    var fTemp=c2F(cTemp)
+    var fTemp=c2F(cTemp);
     //var strWrite="Temperature in Celsius: "+cTemp+ "degrees <br />Temperature in Fahrenheit: "+fTemp+" degrees<br />";
-    var strWrite=cTemp
-    write2Para(paraID, strWrite)
+    var strWrite=cTemp.toString();
+    write2Para(paraID, strWrite);
 }
