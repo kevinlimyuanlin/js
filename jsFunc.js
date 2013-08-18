138e19ebd4121463890ef4f48bcc251ba71f3eb6
Kevin Lim <kevin_raffles@hotmail.com>
2013-08-18 09:03:46 +0000

    fTemp error?

--- jsFunc.js
+++ jsFunc.js
@@ -9,6 +9,6 @@ function write2Para(paraID, text) {
 function writeTemp(paraID, cTemp) {
     var fTemp=c2F(cTemp);
     //var strWrite="Temperature in Celsius: "+cTemp+ "degrees <br />Temperature in Fahrenheit: "+fTemp+" degrees<br />";
-    var strWrite=cTemp.toString();
+    var strWrite=fTemp.toString();
     write2Para(paraID, strWrite);
 }
