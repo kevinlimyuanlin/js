cb5781901886ede5460c026e719b9f82e6554742
Kevin Lim <kevin_raffles@hotmail.com>
2013-08-18 08:44:15 +0000

    debugging

--- jsFunc.js
+++ jsFunc.js
@@ -10,5 +10,5 @@ function writeTemp(paraID, cTemp) {
     var fTemp=c2F(cTemp)
     var strWrite="Temperature in Celsius: "+cTemp+ \
     "degrees/nTemperature in Fahrenheit: "+fTemp+" degrees\n");
-    write2Para(paraID, strWrite)
+    write2Para(paraID, "3")
 }
