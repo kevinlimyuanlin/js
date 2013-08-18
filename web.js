7ff903efd9f91f78d02dd025c102058a067e0ad7
Kevin Lim <kevin_raffles@hotmail.com>
2013-08-18 03:56:16 +0000

    finished lesson 1

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require('fs');
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync('script_1.html', "utf-8"));
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
