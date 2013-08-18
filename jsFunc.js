268d5341ed7f102d481a62887d36a6a4a1f5e99f
Kevin Lim <kevin_raffles@hotmail.com>
2013-08-18 08:40:19 +0000

    calculate temperature better

--- jsFunc.js
+++ jsFunc.js
@@ -1,5 +1,14 @@
 function c2F(value) {
-    var fTemp=value*9/5+32;
-    document.write("Temperature in Celsius: "+value+" degrees<br />");
-    document.write("Temperature in Fahrenheit: "+fTemp+" degrees<br />");
+    return value*9/5+32;
+}
+
+function write2Para(paraID, text) {
+    document.getElementById(paraID).innerHTML=text;
+}
+
+function writeTemp(paraID, cTemp) {
+    var fTemp=c2F(cTemp)
+    var strWrite="Temperature in Celsius: "+cTemp+ \
+    "degrees/nTemperature in Fahrenheit: "+fTemp+" degrees\n");
+    write2Para(paraID, strWrite)
 }
